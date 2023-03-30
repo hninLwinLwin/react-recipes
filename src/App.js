b4b431ed3cf4f8041c8c3697e09e8157c12cfb23
HLL <hninlwinlwin2017@gmail.com>
2023-03-30 10:48:45 +0630

    header or navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Recipes from './pages/Recipes';
+import Default from './pages/Default';
+import SingleRecipes from './pages/SingleRecipes';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/recipes' element={<Recipes/>}/>
-      </Routes>
+      <main>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/recipes' element={<Recipes/>}/>
+          <Route path='/recipes/:id' element={<SingleRecipes/>}/>
+          <Route element={<Default/>}/>
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
